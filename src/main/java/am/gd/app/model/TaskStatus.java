15466e9c9ee9e5dbcdf989bee3e05114c82181f2
Arayik <araharutyunyan@griddynamics.com>
2024-11-29 17:31:49 +0400

    Update main class add repository finish task

--- src/main/java/am/gd/app/model/TaskStatus.java
+++ src/main/java/am/gd/app/model/TaskStatus.java
@@ -4,5 +4,14 @@ public enum TaskStatus {
     NEW,
     IN_PROGRESS,
     COMPLETED,
-    CANCELLED
+    CANCELLED;
+
+    public String toDisplayString() {
+        return switch (this) {
+            case NEW -> "New Task";
+            case IN_PROGRESS -> "In Progress";
+            case COMPLETED -> "Completed";
+            case CANCELLED -> "Cancelled";
+        };
+    }
 }
